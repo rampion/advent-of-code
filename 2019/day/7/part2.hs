079fc05965c5c894ab9ea0b87e3e0d837b9c9d34
Noah Luck Easterly <noah.easterly@gmail.com>
2020-12-01 14:13:06 -0500

    2019 day 7 progress

--- 2019/day/7/part2.hs
+++ 2019/day/7/part2.hs
@@ -58,8 +58,9 @@ xmain =
 -- >>> runAmplifiers [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10] [9,7,8,5,6]
 -- 18217
 runAmplifiers :: Program -> [Value] -> Value
-runAmplifiers v ps = 
-  let xs = foldl (runAmplifier v) (0:xs) ps
+runAmplifiers v ps = runST $ do
+  amps <- forM ps $ startAmplifier v
+  let xs = foldl runAmplifier (1:xs) amps
   in last xs
 
 runAmplifier :: Program -> [Value] -> Value -> [Value]
