20120997e202f928939387091396088c83b25c2c
Noah Luck Easterly <noah.easterly@gmail.com>
2022-12-13 14:48:07 -0500

    overengineering

--- src/AdventOfCode/Day13.hs
+++ src/AdventOfCode/Day13.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeOperators #-}
 module AdventOfCode.Day13 where
 
 import Prelude
@@ -7,30 +8,77 @@ import Text.Parsec hiding ((<|>))
 import Control.Applicative
 import Data.List (sort)
 import GHC.Exts
+import Control.Arrow (first)
 
 newtype Packet = Packet [PacketValue]
   deriving stock (Eq, Ord)
 
+-- for debugging
 instance IsList Packet where
   type Item Packet = PacketValue
   fromList = Packet
   toList (Packet as) = as
 
+-- for debugging
 instance Show Packet where
   show (Packet as) = show as
+
+-- for completeness
+instance Read Packet where
+  readsPrec p = readMap Packet (readsPrec p)
 
 data PacketValue = IntValue Int | ListValue [PacketValue]
   deriving stock (Eq)
 
+-- for debugging
 instance Num PacketValue where
   fromInteger = IntValue . fromInteger
-  (+) = undefined
-  (-) = undefined
-  (*) = undefined
-  abs = undefined
-  signum = undefined
-  negate = undefined
+  -- for completeness
+  (+) = liftO2 (id ? id ?.. (+))
+  (-) = liftO2 (id ? negate ?.. (-))
+  (*) = liftO2 (const 0 ? const 0 ?.. (*))
+  abs = omap abs
+  signum = omap signum
+  negate = omap negate
 
+-- for completeness
+omap :: (Int -> Int) -> PacketValue -> PacketValue
+omap f = \case
+  IntValue a -> IntValue (f a)
+  ListValue as -> ListValue (map (omap f) as)
+
+type (+) = Either
+infixl 6 +
+
+-- opposite fixity of (|||)
+(?) :: (a -> c) -> (b -> c) -> a + b -> c
+(?) = either
+infixl 6 ?
+
+(?..) :: (a -> d) -> (b -> c -> d) -> a + (b, c) -> d
+(?..) f = either f . uncurry
+infixl 6 ?..
+
+-- for completeness
+liftO2 :: (Int + Int + (Int,Int) -> Int) -> PacketValue -> PacketValue -> PacketValue
+liftO2 f = \case
+  IntValue a -> \case
+    IntValue b -> IntValue (f (Right (a, b)))
+    ListValue bs -> ListValue (recur [IntValue a] bs)
+  ListValue as -> \case
+    IntValue b -> ListValue (recur as [IntValue b])
+    ListValue bs -> ListValue (recur as bs)
+  where
+    recur = outerZipWith do 
+      omap (f . Left . Left) ? omap (f . Left . Right) ?.. liftO2 f
+
+-- for completeness
+outerZipWith :: (a + b + (a,b) -> c) -> [a] -> [b] -> [c]
+outerZipWith f [] bs = map (f . Left . Right) bs
+outerZipWith f as [] = map (f . Left . Left) as
+outerZipWith f (a:as) (b:bs) = f (Right (a, b)) : outerZipWith f as bs
+
+-- for debugging
 instance IsList PacketValue where
   type Item PacketValue = PacketValue
   fromList = ListValue
@@ -38,6 +86,7 @@ instance IsList PacketValue where
     ListValue as -> as
     a -> [a]
 
+-- for debugging
 instance Show PacketValue where
   show = \case
     IntValue a -> show a
@@ -47,14 +96,25 @@ instance Ord PacketValue where
   IntValue a `compare` IntValue b = a `compare` b 
   a `compare` b = Packet (toList a) `compare` Packet (toList b)
 
+-- for completeness
+instance Read PacketValue where
+  readsPrec = \p -> readMap IntValue (readsPrec p) <> readMap ListValue (readsPrec p)
+
+-- for completeness
+readMap :: (a -> b) -> ReadS a -> ReadS b
+readMap f = fmap (fmap (first f))
+
 day13parser :: Parser [(Packet,Packet)]
 day13parser = pair `sepBy` newline where
   pair = liftA2 (,) packet packet
+  packet = read <$> some (noneOf "\n") <* newline
+  {-
   packet = Packet <$> values <* newline
   values = char '[' *> (value `sepBy` char ',') <* char ']'
   value = int <|> list
   int = IntValue . read <$> some digit
   list = ListValue <$> values
+  -}
 
 day13part1 :: [(Packet,Packet)] -> Int
 day13part1 = sum . map fst . select . zip [1..] where
