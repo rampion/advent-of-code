9e210bd59f8b4ffb9ede075aa5866ca3086175df
Noah Luck Easterly <noah.easterly@gmail.com>
2023-12-12 17:57:41 -0500

    2023 day 11

--- src/AdventOfCode/Y2023/Day11.hs
+++ src/AdventOfCode/Y2023/Day11.hs
@@ -1,36 +1,86 @@
+{-# LANGUAGE NumericUnderscores #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module AdventOfCode.Y2023.Day11 where
 
 import AdventOfCode.Y2023.Prelude
+import Data.Functor (($>))
+import Data.List (tails, transpose)
+import Data.Bool (bool)
 
 solver :: Solver
 solver = Solver
   { parser
-  , part1
-  , part2
+  , part1 = part2 2
+  , part2 = part2 1_000_000
   , spec = do
-      check parser part1 part2 Example
+      let parsed = 
+            [ [ F, F, F, T, F, F, F, F, F, F ]
+            , [ F, F, F, F, F, F, F, T, F, F ]
+            , [ T, F, F, F, F, F, F, F, F, F ]
+            , [ F, F, F, F, F, F, F, F, F, F ]
+            , [ F, F, F, F, F, F, T, F, F, F ]
+            , [ F, T, F, F, F, F, F, F, F, F ]
+            , [ F, F, F, F, F, F, F, F, F, T ]
+            , [ F, F, F, F, F, F, F, F, F, F ]
+            , [ F, F, F, F, F, F, F, T, F, F ]
+            , [ T, F, F, F, T, F, F, F, F, F ]
+            ]
+      check parser (part2 2) (const ()) Example
         { raw = (<> "\n") [text|
+            ...#......
+            .......#..
+            #.........
+            ..........
+            ......#...
+            .#........
+            .........#
+            ..........
+            .......#..
+            #...#.....
           |]
         , parsed
-        , part1output = ()
+        , part1output = 374
         , part2output = ()
         }
+
+      it "gets the right answer for part two with a factor of 10" do
+        part2 10 parsed `shouldBe` 1030
+
+      it "gets the right answer for part two with a factor of 10" do
+        part2 100 parsed `shouldBe` 8410
   }
 
-parsed :: Input
-parsed = Input
+pattern T :: Bool
+pattern T = True
+
+pattern F :: Bool
+pattern F = False
 
 type Input :: Type
-data Input = Input
-  deriving stock (Show, Eq)
+type Input = [[Bool]]
 
 parser :: Parser Input
-parser = pure Input
+parser = some tile `endBy` newline where
+  tile = asum
+    [ char '.' $> F
+    , char '#' $> T
+    ]
 
-part1 :: Input -> ()
-part1 _ = ()
+distances :: Num a => [(a,a)] -> [a]
+distances pairs = do
+  (y₀,x₀):ps <- tails pairs
+  (y₁,x₁) <- ps
+  pure do
+    abs (y₀ - y₁) + abs (x₀ - x₁)
 
-part2 :: Input -> ()
-part2 _ = ()
+part2 :: Integer -> Input -> Integer
+part2 n = sum . distances . expandedPoints (n - 1)
+
+expandedPoints :: Integer -> Input -> [(Integer,Integer)]
+expandedPoints factor input = do
+  let ys = zipWith (+) [0..] . tail . scanl (+) 0 $ map (bool 0 factor . all not) input
+      xs = zipWith (+) [0..] . tail . scanl (+) 0 . map (bool 0 factor . all not) $ transpose input
+  (y, row) <- zip ys input
+  (x, T) <- zip xs row
+  pure (y,x)
