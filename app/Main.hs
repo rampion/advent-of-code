2eac462f117106baeeb865070cde4192027ddf46
Noah Luck Easterly <noaheasterly@mercury.com>
2024-12-11 09:01:20 -0500

    2024 day 11

--- app/Main.hs
+++ app/Main.hs
@@ -14,18 +14,18 @@
 
 module Main where
 
-import Control.Monad (guard)
+-- import Control.Monad (guard)
 -- import Data.Function ((&))
 -- import Data.Functor ((<&>))
-import Data.List qualified as List
-import Data.Map (Map)
-import Data.Map qualified as Map
-import Data.Maybe (maybeToList)
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Time (fromGregorian)
+-- import Data.List qualified as List
 -- import Data.List.NonEmpty (NonEmpty(..))
 -- import Data.List.NonEmpty qualified as NonEmpty
+-- import Data.Map (Map)
+import Data.Map qualified as Map
+-- import Data.Maybe (maybeToList)
+-- import Data.Set (Set)
+-- import Data.Set qualified as Set
+import Data.Time (fromGregorian)
 import NeatInterpolation (text)
 import SolveDay (solveDay)
 import Test.Hspec (hspec, it, shouldBe)
@@ -37,85 +37,40 @@ import Prelude
 
 main :: IO ()
 main = do
-  solveDay parse1 part2 $ fromGregorian 2024 12 10
+  solveDay parse1 part2 $ fromGregorian 2024 12 11
 
-type Puzzle = [[Height]]
-
-type Height = Int
-
-type X = Int
-
-type Y = Int
+type Puzzle = [Int]
 
 parse1 :: Parser Puzzle
-parse1 = row `endBy1` newline <* eof
+parse1 = stones <* newline <* eof
   where
-    row = many height
-    height = read . pure <$> digit
+    stones = stone `sepBy` char ' '
+    stone = read <$> many digit
 
 part1 :: Puzzle -> Int
-part1 puzz = sum [Set.size (starts Map.! p) | (p, _) <- lasts]
-  where
-    hs = heights puzz
-    (heads, q) = List.span ((== 0) . snd) $ List.sortOn snd hs
-    seed = Map.fromList [(p, Set.singleton p) | (p, _) <- heads]
-    grid = Map.fromList hs
-    starts = bfs grid seed q
-    lasts = List.dropWhile ((< 9) . snd) q
+part1 = countAfter 25
 
 part2 :: Puzzle -> Int
-part2 puzz = sum [trails Map.! p | (p, _) <- lasts]
-  where
-    hs = heights puzz
-    (heads, q) = List.span ((== 0) . snd) $ List.sortOn snd hs
-    seed = Map.fromList [(p, 1) | (p, _) <- heads]
-    grid = Map.fromList hs
-    trails = bfs2 grid seed q
-    lasts = List.dropWhile ((< 9) . snd) q
+part2 = countAfter 75
 
-bfs2 :: Map (X, Y) Height -> Map (X, Y) Int -> [((X, Y), Height)] -> Map (X, Y) Int
-bfs2 grid = foldl' \known (p@(x, y), h) ->
-  Map.insert
-    p
-    do
-      sum do
-        p' <- [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        maybeToList do
-          h' <- Map.lookup p' grid
-          guard $ h' + 1 == h
-          Map.lookup p' known
-    known
+countAfter :: Int -> Puzzle -> Int
+countAfter n puzz = sum do
+  let initial = zip puzz $ repeat 1
+  snd <$> iterate blink initial !! n
 
-bfs :: Map (X, Y) Height -> Map (X, Y) (Set (X, Y)) -> [((X, Y), Height)] -> Map (X, Y) (Set (X, Y))
-bfs grid = foldl' \known (p@(x, y), h) ->
-  Map.insert
-    p
-    do
-      Set.unions do
-        p' <- [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        maybeToList do
-          h' <- Map.lookup p' grid
-          guard $ h' + 1 == h
-          Map.lookup p' known
-    known
+blink :: (Integral stone, Num count) => [(stone, count)] -> [(stone, count)]
+blink = Map.toList . Map.fromListWith (+) . concatMap \(stone, count) -> case stone of
+  0 -> pure (1, count)
+  (numDigits -> w) | even w -> do
+    let (q, r) = stone `quotRem` (10 ^ (w `div` 2))
+    [(q, count), (r, count)]
+  _ -> pure (stone * 2024, count)
 
-heights :: Puzzle -> [((X, Y), Height)]
-heights = concat . zipWith row [0 ..]
-  where
-    row y = zipWith (cell y) [0 ..]
-    cell y x h = ((x, y), h)
+numDigits :: Integral a => a -> a
+numDigits = ceiling @Double . logBase 10 . toEnum . fromEnum . succ
 
 firstExample :: Puzzle
-firstExample =
-  [ [8, 9, 0, 1, 0, 1, 2, 3]
-  , [7, 8, 1, 2, 1, 8, 7, 4]
-  , [8, 7, 4, 3, 0, 9, 6, 5]
-  , [9, 6, 5, 4, 9, 8, 7, 4]
-  , [4, 5, 6, 7, 8, 9, 0, 3]
-  , [3, 2, 0, 1, 9, 0, 1, 2]
-  , [0, 1, 3, 2, 9, 8, 0, 1]
-  , [1, 0, 4, 5, 6, 7, 3, 2]
-  ]
+firstExample = [125, 17]
 
 -- $> runTests
 
@@ -124,21 +79,11 @@ runTests = hspec do
   it "parses the first example" do
     let raw =
           [text|
-            89010123
-            78121874
-            87430965
-            96549874
-            45678903
-            32019012
-            01329801
-            10456732
+            125 17
           |]
             <> "\n"
 
     parse parse1 "first example" raw `shouldBe` Right firstExample
 
   it "solves part one with the first example" do
-    part1 firstExample `shouldBe` 36
-
-  it "solves part two with the first example" do
-    part2 firstExample `shouldBe` 81
+    part1 firstExample `shouldBe` 55312
