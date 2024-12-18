14782a10d84570bca452478654059aed48a4ee31
Noah Luck Easterly <noaheasterly@mercury.com>
2024-12-18 15:55:15 -0500

    2024 day 18

--- app/Main.hs
+++ app/Main.hs
@@ -18,15 +18,36 @@
 
 module Main where
 
-import Control.Monad qualified as Monad
-import Data.Bits (shiftR, xor)
-import Data.Foldable qualified as Foldable
-import Data.Function (on)
-import Data.Maybe qualified as Maybe
+-- import Control.Monad ((>=>))
+-- import Control.Monad qualified as Monad
+-- import Control.Monad.State.Strict (MonadState)
+-- import Control.Monad.State.Strict qualified as MonadState
+-- import Control.Monad.Trans.Maybe (MaybeT (runMaybeT))
+-- import Control.Monad.Trans.Maybe qualified as MaybeT
+-- import Control.Monad.Trans.State.Strict (StateT (runStateT))
+-- import Control.Monad.Trans.Writer.Strict (Writer, runWriter)
+-- import Control.Monad.Writer.Strict qualified as MonadWriter
+-- import Control.Concurrent (threadDelay)
+-- import Data.Bits (xor)
+-- import Data.Foldable qualified as Foldable
+-- import Data.Function ((&))
+-- import Data.Functor ((<&>))
+-- import Data.Functor.Const (Const (Const, getConst))
+-- import Data.Functor.Identity (Identity (Identity, runIdentity))
+-- import Data.Functor.Product (Product (Pair))
+import Data.List qualified as List
+-- import Data.List.NonEmpty (NonEmpty(..))
+-- import Data.List.NonEmpty qualified as NonEmpty
+-- import Data.Map (Map)
+import Data.Map qualified as Map
+-- import Data.Maybe qualified as Maybe
+-- import Data.Ratio ((%))
+-- import Data.Set (Set)
+import Data.Set qualified as Set
 import Data.Time (fromGregorian)
-import Data.Vector (Vector)
-import Data.Vector qualified as Vector
--- import Debug.Trace
+-- import Debug.Trace (traceShow)
+-- import Data.Vector (Vector)
+-- import Data.Vector qualified as Vector
 import NeatInterpolation (text)
 import SolveDay (solveDay)
 import Test.Hspec (hspec, it, shouldBe)
@@ -34,216 +55,158 @@ import Text.Parsec hiding (State)
 import Text.Parsec.Text (Parser)
 import Prelude
 
+-- $> main
+
 main :: IO ()
 main = do
-  solveDay parse1 part2 $ fromGregorian 2024 12 17
+  solveDay parse1 part2 $ fromGregorian 2024 12 18
 
-data Puzzle = Puzzle
-  { registers :: Registers
-  , program :: Program
-  }
-  deriving (Show, Eq)
+type Puzzle = [Point]
 
-data Registers = Registers {a :: Int, b :: Int, c :: Int}
-  deriving (Show, Eq)
+data Point = Point {x :: X, y :: Y}
+  deriving (Eq, Show, Ord)
 
-type Program = [Opcode]
+newtype X = X Int
+  deriving newtype (Show, Eq, Ord, Num)
 
---  0     1     2     3     4     5     6     7
-data Opcode = ADV | BXL | BST | JNZ | BXC | OUT | BDV | CDV
-  deriving (Show, Eq, Ord, Enum, Bounded)
+newtype Y = Y Int
+  deriving newtype (Show, Eq, Ord, Num)
 
 parse1 :: Parser Puzzle
-parse1 = Puzzle <$> registers <* newline <*> program <* eof
+parse1 = points <* eof
   where
-    registers = Registers <$> register 'A' <*> register 'B' <*> register 'C'
-    register c = string "Register " *> char c *> string ": " *> int <* newline
+    points = point `endBy` newline
+    point = Point <$> x <* char ',' <*> y
+    x = X <$> int
+    y = Y <$> int
     int = read <$> many1 digit
-    program = string "Program: " *> sepBy opcode comma <* newline
-    opcode = Foldable.asum [op <$ string (show (fromEnum op)) | op <- [ADV .. CDV]]
-    comma = char ','
 
 firstExample :: Puzzle
 firstExample =
-  Puzzle
-    { registers = Registers {a = 729, b = 0, c = 0}
-    , program = map toEnum [0, 1, 5, 4, 3, 0]
-    }
+  [ Point 5 4
+  , Point 4 2
+  , Point 4 5
+  , Point 3 0
+  , Point 2 1
+  , Point 6 3
+  , Point 2 4
+  , Point 1 5
+  , Point 0 6
+  , Point 3 3
+  , Point 2 6
+  , Point 5 1
+  , Point 1 2
+  , Point 5 5
+  , Point 2 5
+  , Point 6 5
+  , Point 1 4
+  , Point 0 4
+  , Point 6 4
+  , Point 1 1
+  , Point 6 1
+  , Point 1 0
+  , Point 0 5
+  , Point 1 6
+  , Point 2 0
+  ]
 
-part1 :: Puzzle -> [Int]
-part1 Puzzle {program, registers} =
-  generateOutput (Vector.fromList program) registers 0
+part1 :: Puzzle -> Maybe Int
+part1 = part1With 70 70 . take 1024
 
-generateOutput :: Vector Opcode -> Registers -> Int -> [Int]
-generateOutput program = loop
+part1Demo :: Puzzle -> Maybe Int
+part1Demo = part1With 6 6 . take 12
+
+part1With :: X -> Y -> Puzzle -> Maybe Int
+part1With maxX maxY (Set.fromList -> blocked) = loop (Map.singleton start 0) (Map.singleton 0 (Set.singleton start))
   where
-    loop registers p
-      | p + 1 >= Vector.length program = []
-      | otherwise =
-          let literal = fromEnum $ program Vector.! (p + 1)
-              combo = case literal of
-                4 -> a registers
-                5 -> b registers
-                6 -> c registers
-                n -> n
-              p' = p + 2
-              opcode = program Vector.! p
-           in -- in traceShow (p, opcode, literal, registers) case opcode of
-              case opcode of
-                ADV ->
-                  -- divide register a / 2 ^ combo and store in a
-                  loop registers {a = a registers `shiftR` combo} p'
-                BXL ->
-                  -- xor register b with literal
-                  loop registers {b = b registers `xor` literal} p'
-                BST ->
-                  -- store combo mod 8 in register b
-                  loop registers {b = combo `mod` 8} p'
-                JNZ ->
-                  -- jump to literal if register a nonzero
-                  loop registers if a registers == 0 then p' else literal
-                BXC ->
-                  -- xor register b with register c and store in b
-                  loop registers {b = b registers `xor` c registers} p'
-                OUT ->
-                  -- output combo mod 8
-                  (combo `mod` 8) : loop registers p'
-                BDV ->
-                  -- divide register a / 2 ^ combo and store in b
-                  loop registers {b = a registers `shiftR` combo} p'
-                CDV ->
-                  -- divide register a / 2 ^ combo and store in c
-                  loop registers {c = a registers `shiftR` combo} p'
+    start = Point 0 0
+    end = Point maxX maxY
 
-secondExample :: Puzzle
-secondExample =
-  Puzzle
-    { registers = Registers {a = 2024, b = 0, c = 0}
-    , program = map toEnum [0, 3, 5, 4, 3, 0]
-    }
+    loop costs queue = do
+      ((cost, ps), queue) <- Map.minViewWithKey queue
+      if Set.member end ps
+        then pure cost
+        else
+          let cost' = succ cost
+              xs = [(n, mc) | p <- Set.toList ps, n <- neighbors p, let mc = Map.lookup n costs, maybe True (> cost') mc]
+              ns = map fst xs
+              costs' = Map.fromList [(n, cost') | n <- ns] `Map.union` costs
+              queue' = (if null ns then id else Map.insertWith (<>) cost' (Set.fromList ns)) do
+                List.foldl' (\m (n, c) -> Map.adjust (Set.delete n) c m) queue [(n, c) | (n, Just c) <- xs]
+           in loop costs' queue'
 
-day17 :: Puzzle
-day17 =
-  Puzzle
-    { registers = Registers 66171486 0 0
-    , program = map toEnum [2, 4, 1, 6, 7, 5, 4, 6, 1, 4, 5, 5, 0, 3, 3, 0]
-    }
+    neighbors p =
+      filter (not . (`Set.member` blocked)) $
+        concat
+          [ [p {x = p.x - 1} | p.x > 0]
+          , [p {y = p.y - 1} | p.y > 0]
+          , [p {x = p.x + 1} | p.x < maxX]
+          , [p {y = p.y + 1} | p.y < maxY]
+          ]
 
-part2 :: Puzzle -> Maybe Int
-part2 puzzle = Maybe.listToMaybe do
-  let program = Vector.fromList puzzle.program
-      outputs = map fromEnum puzzle.program
-  a <- [0 ..]
-  let eq = (==) `on` take 2
-  Monad.guard do
-    generateOutput program puzzle.registers {a} 0 `eq` outputs
-  pure a
+part2 :: Puzzle -> Maybe Point
+part2 = binarySearch 70 70 =<< length
 
--- $/> program day17
+part2Demo :: Puzzle -> Maybe Point
+part2Demo = binarySearch 6 6 =<< length
 
--- $/> generateOutput (Vector.fromList (program day17)) (registers day17) { a =  233 } 0
+-- $/> part1With 1 1 [Point 1 1]
 
--- $> :set -XNamedFieldPuns
+-- $> binarySearch 6 6 25 firstExample
 
--- $> [(a, o) | hi <- [11367361724445,11367361726493], let x = [2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0], lo <- [0..7], let a = 8 * hi + lo, let o = generateOutput (Vector.fromList (program day17)) (registers day17) { a } 0, o == x]
+binarySearch :: X -> Y -> Int -> Puzzle -> Maybe Point
+binarySearch maxX maxY numPoints ps = loop 0 numPoints
+  where
+    loop lo hi
+      | lo < hi =
+          let i = (lo + hi) `div` 2
+           in if blocked i
+                then loop lo (i - 1)
+                else loop (i + 1) hi
+      | blocked lo = Just (ps !! (lo - 1))
+      | otherwise = Nothing
+
+    blocked i = (== Nothing) . part1With maxX maxY $ take i ps
+
+-- $> runTests
 
 runTests :: IO ()
 runTests = hspec do
   it "parses the first example" do
     let raw =
           [text|
-            Register A: 729
-            Register B: 0
-            Register C: 0
-
-            Program: 0,1,5,4,3,0
+            5,4
+            4,2
+            4,5
+            3,0
+            2,1
+            6,3
+            2,4
+            1,5
+            0,6
+            3,3
+            2,6
+            5,1
+            1,2
+            5,5
+            2,5
+            6,5
+            1,4
+            0,4
+            6,4
+            1,1
+            6,1
+            1,0
+            0,5
+            1,6
+            2,0
           |]
             <> "\n"
 
     parse parse1 "first example" raw `shouldBe` Right firstExample
 
   it "solves part one with the first example" do
-    part1 firstExample `shouldBe` [4, 6, 3, 5, 6, 3, 5, 2, 1, 0]
+    part1Demo firstExample `shouldBe` Just 22
 
-  it "parses the second example" do
-    let raw =
-          [text|
-            Register A: 2024
-            Register B: 0
-            Register C: 0
-
-            Program: 0,3,5,4,3,0
-          |]
-            <> "\n"
-
-    parse parse1 "first example" raw `shouldBe` Right secondExample
-
-  it "solves part two with the second example" do
-    part2 secondExample `shouldBe` Just 117_440
-
-{-
-Register A: 66171486
-Register B: 0
-Register C: 0
-
-Program: 2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0
-
-0: bst 4/a
-  b <- a & 7
-2: bxl 6
-  b ^= 6
-4: cdv 5/b
-  c = a >> b
-6: bxc 6
-  b ^= c
-8: bxl 4
-  b ^= 4
-a: out 5/b
-  out b
-c: adv 3
-  a >>= 3
-e: jnz 0
-
-f a = (a & 7) ^ (a >> ((a & 7) ^ 6)) ^ 2
-
-0 = ( a ^ (a >> (a ^ 6)) ) & 7
-
-2 = f a
-4 = f (a >>> 3)
-2 = f (a >>> 6)
-6 = f (a >>> 9)
-7 = f (a >>> 12)
-5 = f (a >>> 15)
-4 = f (a >>> 18)
-6 = f (a >>> 21)
-1 = f (a >>> 24)
-4 = f (a >>> 27)
-5 = f (a >>> 30)
-5 = f (a >>> 33)
-0 = f (a >>> 36)
-3 = f (a >>> 39)
-3 = f (a >>> 42)
-0 = f (a >>> 45)
-
-a >>> 42 /= 0
-a >>> 45 == 0
-
-  4   3   3   3   3   2   2   2   1   1   1   0   0   0   0
-  2   9   6   3   0   7   4   1   8   5   2   9   6   3   0
-???_???_???_???_???_???_???_???_???_???_???_???_???_???_???
-010_
-010_001_
-010_100_
-
-f a = (a & 7) ^ (a >> ((a & 7) ^ 6)) ^ 2
-  f 0 = 0 ^ (0 >> 6) ^ 2 = 2
-  f 1 = 1 ^ (1 >> 7) ^ 2 = 3
-  f 2 = 2 ^ (2 >> 4) ^ 2 = 0
-  f 3 = 3 ^ (3 >> 5) ^ 2 = 1
-  f 4 = 4 ^ (4 >> 2) ^ 2 = 7
-  f 5 = 5 ^ (5 >> 3) ^ 2 = 7
-  f 6 = 6 ^ (6 >> 0) ^ 2 = 2
-  f 7 = 7 ^ (7 >> 1) ^ 2 = 6
-
-better way to solve it - work backwards output by output
--}
+  it "solves part two with the first example" do
+    part2Demo firstExample `shouldBe` Just Point {x = 6, y = 1}
