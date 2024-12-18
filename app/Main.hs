1043bdcb0b7ce1ced55a8eb10e819895cc768041
Noah Luck Easterly <noaheasterly@mercury.com>
2024-12-18 02:50:34 -0500

    2024 day 17 (manual part 2)

--- app/Main.hs
+++ app/Main.hs
@@ -18,36 +18,15 @@
 
 module Main where
 
-import Control.Monad ((>=>))
 import Control.Monad qualified as Monad
-import Control.Monad.State.Strict (MonadState)
-import Control.Monad.State.Strict qualified as MonadState
-import Control.Monad.Trans.Maybe (MaybeT (runMaybeT))
-import Control.Monad.Trans.Maybe qualified as MaybeT
-import Control.Monad.Trans.State.Strict (StateT (runStateT))
-import Control.Monad.Trans.Writer.Strict (Writer, runWriter)
-import Control.Monad.Writer.Strict qualified as MonadWriter
--- import Control.Concurrent (threadDelay)
-import Data.Bits (xor)
+import Data.Bits (shiftR, xor)
 import Data.Foldable qualified as Foldable
--- import Data.Function ((&))
-import Data.Functor ((<&>))
-import Data.Functor.Const (Const (Const, getConst))
-import Data.Functor.Identity (Identity (Identity, runIdentity))
-import Data.Functor.Product (Product (Pair))
--- import Data.List qualified as List
--- import Data.List.NonEmpty (NonEmpty(..))
--- import Data.List.NonEmpty qualified as NonEmpty
--- import Data.Map (Map)
--- import Data.Map qualified as Map
--- import Data.Maybe qualified as Maybe
--- import Data.Ratio ((%))
--- import Data.Set (Set)
--- import Data.Set qualified as Set
-
+import Data.Function (on)
+import Data.Maybe qualified as Maybe
 import Data.Time (fromGregorian)
 import Data.Vector (Vector)
 import Data.Vector qualified as Vector
+-- import Debug.Trace
 import NeatInterpolation (text)
 import SolveDay (solveDay)
 import Test.Hspec (hspec, it, shouldBe)
@@ -55,11 +34,9 @@ import Text.Parsec hiding (State)
 import Text.Parsec.Text (Parser)
 import Prelude
 
--- $> main
-
 main :: IO ()
 main = do
-  solveDay parse1 part1 $ fromGregorian 2024 12 17
+  solveDay parse1 part2 $ fromGregorian 2024 12 17
 
 data Puzzle = Puzzle
   { registers :: Registers
@@ -72,6 +49,7 @@ data Registers = Registers {a :: Int, b :: Int, c :: Int}
 
 type Program = [Opcode]
 
+--  0     1     2     3     4     5     6     7
 data Opcode = ADV | BXL | BST | JNZ | BXC | OUT | BDV | CDV
   deriving (Show, Eq, Ord, Enum, Bounded)
 
@@ -93,110 +71,81 @@ firstExample =
     }
 
 part1 :: Puzzle -> [Int]
-part1 Puzzle {program, registers} = snd do
-  programLoop (Vector.fromList program) `runProcess` Computer {registers, instructionPointer = 0}
+part1 Puzzle {program, registers} =
+  generateOutput (Vector.fromList program) registers 0
 
-data Computer = Computer
-  { registers :: Registers
-  , instructionPointer :: Int
-  }
-  deriving (Show, Eq)
-
-runProcess :: Process a -> Computer -> ((Maybe a, Computer), [Int])
-runProcess = flip \initial -> runWriter . (`runStateT` initial) . runMaybeT
-
-type Process = MaybeT (StateT Computer (Writer [Int]))
-
-programLoop :: Vector Opcode -> Process ()
-programLoop program = do
-  opcode <- lookupOpcode program =<< postincrementInstructionPointer
-  operand <- lookupOpcode program =<< postincrementInstructionPointer
-  dispatch opcode (fromEnum operand)
-  programLoop program
-
-lookupOpcode :: Vector Opcode -> Int -> Process Opcode
-lookupOpcode program = MaybeT.hoistMaybe . (program Vector.!?)
-
-blankComputer :: Computer
-blankComputer = Computer {instructionPointer = 0, registers = Registers 0 0 0}
-
-postincrementInstructionPointer :: Process Int
-postincrementInstructionPointer =
-  MonadState.state (fromPair . _instructionPointer \i -> Pair (Const i) (Identity (succ i)))
+generateOutput :: Vector Opcode -> Registers -> Int -> [Int]
+generateOutput program = loop
   where
-    fromPair (Pair (Const a) (Identity b)) = (a, b)
+    loop registers p
+      | p + 1 >= Vector.length program = []
+      | otherwise =
+          let literal = fromEnum $ program Vector.! (p + 1)
+              combo = case literal of
+                4 -> a registers
+                5 -> b registers
+                6 -> c registers
+                n -> n
+              p' = p + 2
+              opcode = program Vector.! p
+           in -- in traceShow (p, opcode, literal, registers) case opcode of
+              case opcode of
+                ADV ->
+                  -- divide register a / 2 ^ combo and store in a
+                  loop registers {a = a registers `shiftR` combo} p'
+                BXL ->
+                  -- xor register b with literal
+                  loop registers {b = b registers `xor` literal} p'
+                BST ->
+                  -- store combo mod 8 in register b
+                  loop registers {b = combo `mod` 8} p'
+                JNZ ->
+                  -- jump to literal if register a nonzero
+                  loop registers if a registers == 0 then p' else literal
+                BXC ->
+                  -- xor register b with register c and store in b
+                  loop registers {b = b registers `xor` c registers} p'
+                OUT ->
+                  -- output combo mod 8
+                  (combo `mod` 8) : loop registers p'
+                BDV ->
+                  -- divide register a / 2 ^ combo and store in b
+                  loop registers {b = a registers `shiftR` combo} p'
+                CDV ->
+                  -- divide register a / 2 ^ combo and store in c
+                  loop registers {c = a registers `shiftR` combo} p'
 
-dispatch :: Opcode -> Int -> Process ()
-dispatch = \case
-  ADV -> adv -- divide register a / 2 ^ combo
-  BXL -> bxl -- xor register b with literal
-  BST -> bst -- store combo mod 8 in register b
-  JNZ -> jnz -- jump to literal if register a nonzero
-  BXC -> bxc -- xor register b with register c and store in b
-  OUT -> out -- output combo mod 8
-  BDV -> bdv -- divide register / 2 ^ combo and store in b
-  CDV -> cdv -- divide register / 2 ^ combo and store in c
+secondExample :: Puzzle
+secondExample =
+  Puzzle
+    { registers = Registers {a = 2024, b = 0, c = 0}
+    , program = map toEnum [0, 3, 5, 4, 3, 0]
+    }
 
-adv, bxl, bst, jnz, bxc, out, bdv, cdv :: Int -> Process ()
-adv = rdv _a
-bdv = rdv _b
-cdv = rdv _c
+day17 :: Puzzle
+day17 =
+  Puzzle
+    { registers = Registers 66171486 0 0
+    , program = map toEnum [2, 4, 1, 6, 7, 5, 4, 6, 1, 4, 5, 5, 0, 3, 3, 0]
+    }
 
-rdv :: Setter' Computer Int -> Int -> Process ()
-rdv reg =
-  combo >=> \denom -> do
-    numer <- use _a
-    reg .= (numer `div` 2 ^ denom)
+part2 :: Puzzle -> Maybe Int
+part2 puzzle = Maybe.listToMaybe do
+  let program = Vector.fromList puzzle.program
+      outputs = map fromEnum puzzle.program
+  a <- [0 ..]
+  let eq = (==) `on` take 2
+  Monad.guard do
+    generateOutput program puzzle.registers {a} 0 `eq` outputs
+  pure a
 
-bxl n = _b %= (`xor` n)
+-- $/> program day17
 
-bxc _ = bxl =<< use _c
+-- $/> generateOutput (Vector.fromList (program day17)) (registers day17) { a =  233 } 0
 
-bst = combo >=> \n -> _b .= (n `mod` 8)
+-- $> :set -XNamedFieldPuns
 
-jnz n = do
-  a <- use _a
-  Monad.when (a /= 0) do
-    _instructionPointer .= n
-
-out = combo >=> \n -> MonadWriter.tell [n `mod` 8]
-
-combo :: Int -> Process Int
-combo = \case
-  4 -> use _a
-  5 -> use _b
-  6 -> use _c
-  n -> pure n
-
-_a, _b, _c, _instructionPointer :: Lens' Computer Int
-_a f computer = f computer.registers.a <&> \a -> (computer :: Computer) {registers = computer.registers {a}}
-_b f computer = f computer.registers.b <&> \b -> (computer :: Computer) {registers = computer.registers {b}}
-_c f computer = f computer.registers.c <&> \c -> (computer :: Computer) {registers = computer.registers {c}}
-_instructionPointer f computer =
-  f computer.instructionPointer <&> \instructionPointer ->
-    computer {instructionPointer}
-
-type Optic' f s a = (a -> f a) -> (s -> f s)
-
-type Lens' s a = forall f. (Functor f) => Optic' f s a
-
-type Setter' s a = Optic' Identity s a
-
-type Getter' s a = Optic' (Const a) s a
-
-(%=) :: (MonadState s m) => Setter' s a -> (a -> a) -> m ()
-l %= f = MonadState.modify (runIdentity . l (Identity . f))
-
-(.=) :: (MonadState s m) => Setter' s a -> a -> m ()
-l .= a = l %= const a
-
-use :: (MonadState s m) => Getter' s a -> m a
-use l = MonadState.gets (getConst . l Const)
-
-part2 :: Puzzle -> Int
-part2 _ = 0
-
--- $> runTests
+-- $> [(a, o) | hi <- [11367361724445,11367361726493], let x = [2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0], lo <- [0..7], let a = 8 * hi + lo, let o = generateOutput (Vector.fromList (program day17)) (registers day17) { a } 0, o == x]
 
 runTests :: IO ()
 runTests = hspec do
@@ -216,5 +165,85 @@ runTests = hspec do
   it "solves part one with the first example" do
     part1 firstExample `shouldBe` [4, 6, 3, 5, 6, 3, 5, 2, 1, 0]
 
-  it "solves part two with the first example" do
-    part2 firstExample `shouldBe` 0
+  it "parses the second example" do
+    let raw =
+          [text|
+            Register A: 2024
+            Register B: 0
+            Register C: 0
+
+            Program: 0,3,5,4,3,0
+          |]
+            <> "\n"
+
+    parse parse1 "first example" raw `shouldBe` Right secondExample
+
+  it "solves part two with the second example" do
+    part2 secondExample `shouldBe` Just 117_440
+
+{-
+Register A: 66171486
+Register B: 0
+Register C: 0
+
+Program: 2,4,1,6,7,5,4,6,1,4,5,5,0,3,3,0
+
+0: bst 4/a
+  b <- a & 7
+2: bxl 6
+  b ^= 6
+4: cdv 5/b
+  c = a >> b
+6: bxc 6
+  b ^= c
+8: bxl 4
+  b ^= 4
+a: out 5/b
+  out b
+c: adv 3
+  a >>= 3
+e: jnz 0
+
+f a = (a & 7) ^ (a >> ((a & 7) ^ 6)) ^ 2
+
+0 = ( a ^ (a >> (a ^ 6)) ) & 7
+
+2 = f a
+4 = f (a >>> 3)
+2 = f (a >>> 6)
+6 = f (a >>> 9)
+7 = f (a >>> 12)
+5 = f (a >>> 15)
+4 = f (a >>> 18)
+6 = f (a >>> 21)
+1 = f (a >>> 24)
+4 = f (a >>> 27)
+5 = f (a >>> 30)
+5 = f (a >>> 33)
+0 = f (a >>> 36)
+3 = f (a >>> 39)
+3 = f (a >>> 42)
+0 = f (a >>> 45)
+
+a >>> 42 /= 0
+a >>> 45 == 0
+
+  4   3   3   3   3   2   2   2   1   1   1   0   0   0   0
+  2   9   6   3   0   7   4   1   8   5   2   9   6   3   0
+???_???_???_???_???_???_???_???_???_???_???_???_???_???_???
+010_
+010_001_
+010_100_
+
+f a = (a & 7) ^ (a >> ((a & 7) ^ 6)) ^ 2
+  f 0 = 0 ^ (0 >> 6) ^ 2 = 2
+  f 1 = 1 ^ (1 >> 7) ^ 2 = 3
+  f 2 = 2 ^ (2 >> 4) ^ 2 = 0
+  f 3 = 3 ^ (3 >> 5) ^ 2 = 1
+  f 4 = 4 ^ (4 >> 2) ^ 2 = 7
+  f 5 = 5 ^ (5 >> 3) ^ 2 = 7
+  f 6 = 6 ^ (6 >> 0) ^ 2 = 2
+  f 7 = 7 ^ (7 >> 1) ^ 2 = 6
+
+better way to solve it - work backwards output by output
+-}
