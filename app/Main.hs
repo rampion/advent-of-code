8e234528b9c1bc071c3b0cc20d6c2c3ef2837b57
Noah Luck Easterly <noaheasterly@mercury.com>
2024-12-20 17:11:11 -0500

    2024 day 20

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -Wextra -Werror -Wno-name-shadowing -Wno-ambiguous-fields #-}
@@ -29,21 +30,21 @@ import Control.Monad qualified as Monad
 -- import Control.Monad.Writer.Strict qualified as MonadWriter
 -- import Control.Concurrent (threadDelay)
 -- import Data.Bits (xor)
--- import Data.Foldable qualified as Foldable
+import Data.Foldable qualified as Foldable
 -- import Data.Function ((&))
 -- import Data.Functor ((<&>))
 -- import Data.Functor.Const (Const (Const, getConst))
 -- import Data.Functor.Identity (Identity (Identity, runIdentity))
 -- import Data.Functor.Product (Product (Pair))
-import Data.List qualified as List
+-- import Data.List qualified as List
 -- import Data.List.NonEmpty (NonEmpty(..))
 -- import Data.List.NonEmpty qualified as NonEmpty
 import Data.Map (Map)
 import Data.Map qualified as Map
--- import Data.Maybe qualified as Maybe
+import Data.Maybe qualified as Maybe
 -- import Data.Ratio ((%))
 -- import Data.Set (Set)
--- import Data.Set qualified as Set
+import Data.Set qualified as Set
 import Data.Time (fromGregorian)
 -- import Debug.Trace (traceShow)
 -- import Data.Vector (Vector)
@@ -57,52 +58,145 @@ import Prelude
 
 main :: IO ()
 main = do
-  solveDay parse1 part2 $ fromGregorian 2024 12 19
+  solveDay parse1 part2 $ fromGregorian 2024 12 20
 
-data Puzzle = Puzzle
-  { towels :: [String]
-  , designs :: [String]
-  }
+type Puzzle = [[Tile]]
+
+data Tile = Wall | Track | Start | End
   deriving (Show, Eq)
 
 parse1 :: Parser Puzzle
-parse1 = Puzzle <$> towels <* newline <* newline <*> designs <* eof
+parse1 = rows <* eof
   where
-    towels = pattern `sepBy1` string ", "
-    designs = pattern `endBy1` newline
-    pattern = many1 lower
+    rows = many1 tile `endBy1` newline
+    tile =
+      Foldable.asum
+        [ Wall <$ char '#'
+        , Track <$ char '.'
+        , Start <$ char 'S'
+        , End <$ char 'E'
+        ]
 
 firstExample :: Puzzle
 firstExample =
-  Puzzle
-    { towels = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
-    , designs = ["brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb"]
-    }
-
-part1 :: Puzzle -> Int
-part1 Puzzle {towels, designs} = length . filter (> 0) $ map (combos towels) designs
-
-combos :: [String] -> String -> Int
-combos pats design = cache Map.! 0
-  where
-    cache :: Map Int Int
-    cache = Map.fromList do
-      (off, suf) <- zip [0 ..] $ List.tails design
-      pure . (,) off . sum $
-        if null suf
-          then do
-            pure 1
-          else do
-            (len, pat) <- lenpats
-            Monad.guard $ pat `List.isPrefixOf` suf
-            pure $ cache Map.! (off + len)
-
-    lenpats = [(length pat, pat) | pat <- pats]
-
-part2 :: Puzzle -> Int
-part2 Puzzle {towels, designs} = sum $ combos towels <$> designs
+  [ [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall]
+  , [Wall, Track, Track, Track, Wall, Track, Track, Track, Wall, Track, Track, Track, Track, Track, Wall]
+  , [Wall, Track, Wall, Track, Wall, Track, Wall, Track, Wall, Track, Wall, Wall, Wall, Track, Wall]
+  , [Wall, Start, Wall, Track, Track, Track, Wall, Track, Wall, Track, Wall, Track, Track, Track, Wall]
+  , [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Track, Wall, Track, Wall, Track, Wall, Wall, Wall]
+  , [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Track, Wall, Track, Wall, Track, Track, Track, Wall]
+  , [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Track, Wall, Track, Wall, Wall, Wall, Track, Wall]
+  , [Wall, Wall, Wall, Track, Track, End, Wall, Track, Track, Track, Wall, Track, Track, Track, Wall]
+  , [Wall, Wall, Wall, Track, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Track, Wall, Wall, Wall]
+  , [Wall, Track, Track, Track, Wall, Wall, Wall, Track, Track, Track, Wall, Track, Track, Track, Wall]
+  , [Wall, Track, Wall, Wall, Wall, Wall, Wall, Track, Wall, Track, Wall, Wall, Wall, Track, Wall]
+  , [Wall, Track, Wall, Track, Track, Track, Wall, Track, Wall, Track, Wall, Track, Track, Track, Wall]
+  , [Wall, Track, Wall, Track, Wall, Track, Wall, Track, Wall, Track, Wall, Track, Wall, Wall, Wall]
+  , [Wall, Track, Track, Track, Wall, Track, Track, Track, Wall, Track, Track, Track, Wall, Wall, Wall]
+  , [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall]
+  ]
 
 -- $> main
+
+part1 :: Puzzle -> Int
+part1 = sum . countShortcuts 2 100
+
+part2 :: Puzzle -> Int
+part2 = sum . countShortcuts 20 100
+
+countShortcuts :: Int -> Int -> Puzzle -> Map Int Int
+countShortcuts n l = Map.fromListWith (+) . map (,1) . allShortcuts n l
+
+allShortcuts :: Int -> Int -> Puzzle -> [Int]
+allShortcuts n l = (shortcuts n l <*> distances) . course
+
+data Direction = North | South | East | West
+
+next :: Direction -> [Direction]
+next = \case
+  North -> [North, East, West]
+  South -> [South, East, West]
+  East -> [East, North, South]
+  West -> [West, North, South]
+
+step :: Direction -> Point -> Point
+step = flip \p -> \case
+  North -> p {y = p.y - 1}
+  South -> p {y = p.y + 1}
+  West -> p {x = p.x - 1}
+  East -> p {x = p.x + 1}
+
+printShortcut :: Puzzle -> [Point] -> IO ()
+printShortcut rows skips = putStr . unlines $ do
+  let codes = Map.fromList $ zip skips ['1' ..]
+  (y, cols) <- zip [0 ..] rows
+  pure do
+    (x, tile) <- zip [0 ..] cols
+    let p = Point x y
+    pure case tile of
+      _ | Just code <- Map.lookup p codes -> code
+      Wall -> '#'
+      Start -> 'S'
+      End -> 'E'
+      Track -> '.'
+
+shortcuts :: Int -> Int -> Course -> Map Point Int -> [Int]
+shortcuts n l _course distances = Map.elems $ Map.fromListWith max do
+  (p, d) <- Map.toList distances
+  dy <- [0 .. n]
+  dx <- [0 .. n - dy]
+  y <- [p.y - Y dy, p.y + Y dy]
+  x <- [p.x - X dx, p.x + X dx]
+  let p' = Point {x, y}
+  d' <- Maybe.maybeToList $ Map.lookup p' distances
+  let saved = d' - d - dy - dx
+  Monad.guard $ saved >= l
+  pure ((p, p'), saved)
+
+data Course = Course
+  { start :: Point
+  , end :: Point
+  , walls :: Map Point Bool
+  }
+
+data Point = Point {x :: X, y :: Y}
+  deriving (Eq, Show, Ord)
+
+newtype X = X {getX :: Int}
+  deriving newtype (Eq, Show, Ord, Enum, Num, Real, Integral)
+
+newtype Y = Y {getY :: Int}
+  deriving newtype (Eq, Show, Ord, Enum, Num, Real, Integral)
+
+distances :: Course -> Map Point Int
+distances course = loop (Map.singleton course.end 0) (Set.singleton course.end) 1
+  where
+    loop ds ps !d =
+      let update = Map.fromList do
+            p <- Set.toList ps
+            n <- (`step` p) <$> [North, South, East, West]
+            Monad.guard do
+              Just False == Map.lookup n course.walls && not (Map.member n ds)
+            pure (n, d)
+       in if Set.null ps
+            then ds
+            else loop (Map.union ds update) (Map.keysSet update) (d + 1)
+
+course :: Puzzle -> Course
+course rows =
+  Course
+    { start = only [p | (p, Start) <- labeled]
+    , end = only [p | (p, End) <- labeled]
+    , walls = Map.fromList [(p, t == Wall) | (p, t) <- labeled]
+    }
+  where
+    only = \case
+      [a] -> a
+      _ -> error "no unique element"
+    labeled = do
+      (y, cols) <- zip [0 ..] rows
+      (x, tile) <- zip [0 ..] cols
+      pure (Point {x, y}, tile)
 
 -- $> runTests
 
@@ -111,23 +205,28 @@ runTests = hspec do
   it "parses the first example" do
     let raw =
           [text|
-            r, wr, b, g, bwu, rb, gb, br
-
-            brwrr
-            bggr
-            gbbr
-            rrbgbr
-            ubwu
-            bwurrg
-            brgr
-            bbrgwb
+            ###############
+            #...#...#.....#
+            #.#.#.#.#.###.#
+            #S#...#.#.#...#
+            #######.#.#.###
+            #######.#.#...#
+            #######.#.###.#
+            ###..E#...#...#
+            ###.#######.###
+            #...###...#...#
+            #.#####.#.###.#
+            #.#...#.#.#...#
+            #.#.#.#.#.#.###
+            #...#...#...###
+            ###############
           |]
             <> "\n"
 
     parse parse1 "first example" raw `shouldBe` Right firstExample
 
   it "solves part one with the first example" do
-    part1 firstExample `shouldBe` 6
+    countShortcuts 2 1 firstExample `shouldBe` Map.fromList [(2, 14), (4, 14), (6, 2), (8, 4), (10, 2), (12, 3), (20, 1), (36, 1), (38, 1), (40, 1), (64, 1)]
 
   it "solves part two with the first example" do
-    part2 firstExample `shouldBe` 16
+    countShortcuts 20 50 firstExample `shouldBe` Map.fromList [(50, 32), (52, 31), (54, 29), (56, 39), (58, 25), (60, 23), (62, 20), (64, 19), (66, 12), (68, 14), (70, 12), (72, 22), (74, 4), (76, 3)]
